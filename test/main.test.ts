f002427db934919b9b87e87fc9741846d3ad21ba
Jinyoung Kim <jingood2@Jinyoungui-MacBookAir.local>
2021-11-04 06:17:16 +0900

    Fix test

--- test/main.test.ts
+++ test/main.test.ts
@@ -7,6 +7,6 @@ test('Snapshot', () => {
   const app = new App();
   const stack = new MyStack(app, 'test');
 
-  expect(stack).not.toHaveResource('AWS::S3::Bucket');
+  expect(stack).toHaveResource('AWS::S3::Bucket');
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
